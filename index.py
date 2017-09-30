9565a434a9a4f1042deff096363f3102fd9eb323
Levy Shi <levyshi@buffalo.edu>
2017-09-29 23:34:42 -0400

    yelp API

--- index.py
+++ index.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from option import Option
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+
+    text = request.form['text']
+    cityName = text.upper()
+    findCity = Option()
+    param = findCity.city_search(cityName)
+    return str(findCity.get_results(param))
+
 
 if __name__ == "__main__":
 
---
+++ option.py
@@ -0,0 +1,25 @@
+import requests
+import pprint
+import rauth
+import time
+
+class Option:
+    @classmethod
+    def city_search(cls,city):
+        params = {}
+        params["location"] = city
+        return params
+
+    @classmethod
+    def get_results(cls,params):
+        consumer_key = "a5QvhItx614Et3Zy6H5qHQ"
+        consumer_secret = "sTAAmFINqdlrKhlNfJbBa3FzfTjbI5WvBh1s643EY5zj7dgnx0m7BKXSNZdjV4RF"
+        data = {'grant_type': 'client_credentials',
+            'client_id': consumer_key,
+            'client_secret': consumer_secret}
+        token = requests.post('https://api.yelp.com/oauth2/token', data=data)
+        access_token = token.json()['access_token']
+        url = 'https://api.yelp.com/v3/businesses/search'
+        headers = {'Authorization': 'bearer %s' % access_token}
+        resp = requests.get(url=url, params=params, headers=headers)
+        return resp.json()
